269fb0233158a2cbbcbcaedc0bd62f4cd3a1ef6d
Jaromír Svoboda <svobod8@gmail.com>
2013-02-03 20:25:20 +0100

    basic ffa chat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prattle::Application.config.secret_token = '050bf79177a9474fdbee05b101e75dc582b232a660eec3a83ee4c8a79406c4644cd6092f6effaa2236b21491cd77bda1ef902e59763a53304a09ae848be89bd8'
